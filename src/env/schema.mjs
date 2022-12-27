cef3a815d91d56418840bc40852fb20a3ebf7bf4
ElasticBottle <winstonyeo99@yahoo.com>
2022-12-27 05:24:53 -0500

    Add redis Add DB and redis env vars Destructure server env vars to support middleware

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -7,7 +7,9 @@ import { z } from "zod";
  */
 export const serverSchema = z.object({
   DATABASE_URL: z.string().url(),
+  DATABASE_SHADOW_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
+  JWT_SECRET: z.string().min(1),
   NEXTAUTH_SECRET:
     process.env.NODE_ENV === "production"
       ? z.string().min(1)
@@ -17,11 +19,28 @@ export const serverSchema = z.object({
     // Since NextAuth.js automatically uses the VERCEL_URL if present.
     (str) => process.env.VERCEL_URL ?? str,
     // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-    process.env.VERCEL ? z.string() : z.string().url(),
+    process.env.VERCEL ? z.string() : z.string().url()
   ),
   DISCORD_CLIENT_ID: z.string(),
   DISCORD_CLIENT_SECRET: z.string(),
 });
+
+/**
+ * You can't destruct `process.env` as a regular object, so you have to do
+ * it manually here. This is because Next.js evaluates this at build time,
+ * and only used environment variables are included in the build.
+ * @type {{ [k in keyof z.infer<typeof serverSchema>]: z.infer<typeof serverSchema>[k] | undefined }}
+ */
+export const serverEnv = {
+  DATABASE_URL: process.env.DATABASE_URL,
+  DATABASE_SHADOW_URL: process.env.DATABASE_SHADOW_URL,
+  NODE_ENV: process.env.NODE_ENV,
+  JWT_SECRET: process.env.JWT_SECRET,
+  NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+  NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
+  DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
+};
 
 /**
  * Specify your client-side environment variables schema here.
@@ -29,7 +48,8 @@ export const serverSchema = z.object({
  * To expose them to the client, prefix them with `NEXT_PUBLIC_`.
  */
 export const clientSchema = z.object({
-  // NEXT_PUBLIC_CLIENTVAR: z.string(),
+  NEXT_PUBLIC_UPSTASH_REDIS_REST_URL: z.string(),
+  NEXT_PUBLIC_UPSTASH_REDIS_REST_TOKEN: z.string(),
 });
 
 /**
@@ -39,5 +59,8 @@ export const clientSchema = z.object({
  * @type {{ [k in keyof z.infer<typeof clientSchema>]: z.infer<typeof clientSchema>[k] | undefined }}
  */
 export const clientEnv = {
-  // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+  NEXT_PUBLIC_UPSTASH_REDIS_REST_URL:
+    process.env.NEXT_PUBLIC_UPSTASH_REDIS_REST_URL,
+  NEXT_PUBLIC_UPSTASH_REDIS_REST_TOKEN:
+    process.env.NEXT_PUBLIC_UPSTASH_REDIS_REST_TOKEN,
 };
